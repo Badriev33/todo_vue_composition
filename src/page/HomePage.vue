82dca4e6c09198564046693a87036a46f355b4d7
Avas <avasbadriev@mail.ru>
2023-07-22 00:06:45 +0400

    tiilwind install

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -1,11 +1,14 @@
 <script setup>
-//import { useRouter } from 'vue-router';
-//const router = useRouter()
+import { useRouter } from 'vue-router';
+const router = useRouter()
 
 </script>
 
 <template>
-    <div>
-        <h1>Hello is Home</h1>
+    <div class="flex flex-col justify-center items-center w-full h-full">
+        <h1>Тестовый проект "To do List"</h1>
+        <button class="h-10 rounded-md bg-black text-white" @click="router.push('List')" type="button">
+            Перейти к списку дел
+        </button>
     </div>
 </template>
